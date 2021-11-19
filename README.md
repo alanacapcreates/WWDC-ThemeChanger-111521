53f3055322b8a03df32ab8793ad50635b8861cf0
Alana Caporale <38472545+alanacapcreates@users.noreply.github.com>
2021-11-19 07:19:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WWDC-ThemeChanger-111521
+ 
---
+++ index.js
@@ -0,0 +1,20 @@
+// ✨ You got this!
+
+let toggleBtn = document.getElementById("toggle")
+
+toggleBtn.addEventListener('click',toggleTheme)
+
+ 
+function toggleTheme() {
+            
+            let currentTheme = document.getElementById('styleeee');
+            if(toggleBtn.checked == true){
+                currentTheme.setAttribute('href', "dark.css");
+            }
+            else{
+                currentTheme.setAttribute('href', "index.css");
+            }
+            
+        
+            
+        }
