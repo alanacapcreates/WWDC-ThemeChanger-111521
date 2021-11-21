ec2efa2bd4daa7d1ae0d7bbfec2f4df73f6ac776
Alana Caporale <38472545+alanacapcreates@users.noreply.github.com>
2021-11-21 15:33:12 -0500

    added click event listener

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ let toggleBtn = document.getElementById("toggle")
 
 toggleBtn.addEventListener('click',toggleTheme)
 
- 
+let buttons = document.getElementsByTagName('button')
+// console.log(buttons)
+let btnLinks = ['https://www.space.com/beaver-moon-lunar-eclipse-photos-november-2021','https://www.space.com/nasa-james-webb-space-telescope-launch-one-month','https://www.space.com/wormholes-may-be-stable-after-all']
+
+buttons[0].addEventListener('click',()=>{window.open(btnLinks[0],'_blank')})
+
+buttons[1].addEventListener('click',()=>{window.open(btnLinks[1],'_blank')})
+
+buttons[2].addEventListener('click',()=>{window.open(btnLinks[2],'_blank')})
+
 function toggleTheme() {
             
             let currentTheme = document.getElementById('styleeee');
